eb8de9b1b0a207914403212e087ec2608aca1432
Inigo Hersanta Sutandyo <86969846+inigoSutandyo@users.noreply.github.com>
2023-02-03 10:55:42 +0700

    add: navigation drawer

--- app/src/main/java/com/example/cinemacgp/activity/MainActivity.java
+++ app/src/main/java/com/example/cinemacgp/activity/MainActivity.java
@@ -1,18 +1,57 @@
 package com.example.cinemacgp.activity;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 
 import com.example.cinemacgp.R;
 import com.example.cinemacgp.controller.MovieController;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActionBarDrawerToggle actionBarDrawerToggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initNavigation();
+        selectItem();
+
         MovieController.fetchTop(this);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void initNavigation() {
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+    }
+
+    private void selectItem() {
+        NavigationView navigationView = findViewById(R.id.navigationView);
+        navigationView.setNavigationItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.movies_nav) {
+
+            } else if (item.getItemId() == R.id.cinemas_nav) {
+
+            }
+            return true;
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
